022b83b8eeb681804aca038815e0e13156f94d20
Tim Clemans <tclemans@sagemath.com>
2016-10-09 18:34:14 +0000

    install nginx default

--- scripts/install/install.sh
+++ scripts/install/install.sh
@@ -3,3 +3,6 @@ source /etc/lsb-release && echo "deb http://download.rethinkdb.com/apt $DISTRIB_
 wget -qO- https://download.rethinkdb.com/apt/pubkey.gpg | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install -y rethinkdb
+ABSOLUTE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+sudo cp $ABSOLUTE_DIR/nginx_default.txt /etc/nginx/default /etc/nginx/sites-available/default
+sudo service nginx restart
