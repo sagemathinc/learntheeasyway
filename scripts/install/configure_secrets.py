e924196b72bd9cd4297a47e5f7a45bab63f11b15
Tim Clemans <tclemans@sagemath.com>
2016-10-09 21:17:24 +0000

    create secrets.toml

--- scripts/install/configure_secrets.py
+++ scripts/install/configure_secrets.py
@@ -1,5 +1,17 @@
 import os.path
 import os
-current_dir = os.path.dirname(os.path.abspath(__file__))
+import random
+import string
 
-os.path.isfile(os.path.join(current_dir, fname))
+scripts_dir = os.path.dirname(os.path.abspath(__file__))
+secrets_path = os.path.join(scripts_dir, '../..', '.hz', 'secrets.toml')
+secrets_template_path = os.path.join(scripts_dir, 'secrets_template.toml')
+if not os.path.isfile(fpath):
+    with open(secrets_template_path, 'r') as f:
+        secrets_toml = f.read()
+    # generate secret token
+    N = 20
+    token = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(N))
+    secrets_toml = 'token_secret = "%s"\n' % (token) + secrets_toml
+    with open(secrets_path, 'w') as f:
+        f.write(secrets_toml)
