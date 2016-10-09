207400ac1a19f541ab4cbb8a5ec4de3b1447f34b
Tim Clemans <tclemans@sagemath.com>
2016-10-09 18:37:57 +0000

    fix typo

--- scripts/install/install.sh
+++ scripts/install/install.sh
@@ -4,5 +4,5 @@ wget -qO- https://download.rethinkdb.com/apt/pubkey.gpg | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install -y rethinkdb
 ABSOLUTE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-sudo cp $ABSOLUTE_DIR/nginx_default.txt /etc/nginx/default /etc/nginx/sites-available/default
+sudo cp $ABSOLUTE_DIR/nginx_default.txt /etc/nginx/sites-available/default
 sudo service nginx restart
