bf8167a03711e69146ff92436b49a547e74e3d7c
Tim Clemans <tclemans@sagemath.com>
2016-10-10 01:45:24 +0000

    fix add ssl cert creation to install script

--- scripts/install/install.sh
+++ scripts/install/install.sh
@@ -4,7 +4,7 @@ wget -qO- https://download.rethinkdb.com/apt/pubkey.gpg | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install -y rethinkdb
 ABSOLUTE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-if [ ! -f /etc/ssl/private/nginx-selfsigned.key ]; then
+if [ ! -f /etc/ssl/certs/nginx-selfsigned.crt ]; then
     sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt -subj "/C=GB/ST=London/L=London/O=Global Security/OU=IT Department/CN=example.com"
     sudo openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
     sudo cp $ABSOLUTE_DIR/nginx_default.txt /etc/nginx/sites-available/default
